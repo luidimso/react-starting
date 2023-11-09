f29115566f7b0c82831a34453b11d1716550418a
luidimso <luidi_mso@outlook.com>
2023-11-09 12:01:40 -0300

    Generate a array of components dynamicly

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}></CoreConcept>
-            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            {CORE_CONCEPTS.map((component) => <CoreConcept {...component}></CoreConcept>)}
           </ul>
         </section>
         <h2>Time to get started!</h2>
