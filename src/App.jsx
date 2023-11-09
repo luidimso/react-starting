446e7449ae2131c1c0d8dc5c63cfc360d9cecc29
luidimso <luidi_mso@outlook.com>
2023-11-09 05:54:08 -0300

    Dynamicly calling Javascript function inside HTML (JSX)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+const d = new Date();
+
 function Header() {
   return (
     <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {d.getDate()} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
