e4efa361b0b77a2d18f6eee8b19b0e48c1d7eaf2
luidimso <luidi_mso@outlook.com>
2023-11-09 06:14:05 -0300

    Passings properties to components

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,25 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <CoreConcept title="Test 1" description="Test 1: text"></CoreConcept>
+          <CoreConcept title="Test 2" description="Test 2: text"></CoreConcept>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
