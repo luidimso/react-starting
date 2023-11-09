2b6aaacdb4d44d54954c325a4c0ad8d059f04501
luidimso <luidi_mso@outlook.com>
2023-11-09 05:58:47 -0300

    IMporting and using a image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import img from "./assets/react-core-concepts.png";
+
 const d = new Date();
 
 function Header() {
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={img} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {d.getDate()} React concepts you will need for almost any app you are
